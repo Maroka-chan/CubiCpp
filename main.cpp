339cc9a7c1c99fcc664a79c9fee544284020987e
Maroka-chan <64618598+Maroka-chan@users.noreply.github.com>
2022-07-10 00:44:15 +0200

    Create vulkan instance

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
+#include <vector>
+#include <algorithm>
 
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
@@ -19,6 +21,7 @@ public:
 
 private:
     GLFWwindow* window;
+    VkInstance instance;
 
     void initWindow() {
         glfwInit();
@@ -29,7 +32,7 @@ private:
     }
 
     void initVulkan() {
-
+        createInstance();
     }
 
     void mainLoop() {
@@ -39,9 +42,84 @@ private:
     }
 
     void cleanup() {
+        vkDestroyInstance(instance, nullptr);
+
         glfwDestroyWindow(window);
 
         glfwTerminate();
+    }
+
+    void createInstance() {
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "CubiCpp";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+        createInfo.enabledLayerCount = 0;
+
+        if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+            throw std::runtime_error("failed to create instance!");
+        }
+
+        uint32_t extensionCount = 0;
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+        std::vector<VkExtensionProperties> extensions(extensionCount);
+
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+
+        std::cout << "available extensions:\n";
+
+        for (const auto& extension : extensions) {
+            std::cout << '\t' << extension.extensionName << '\n';
+        }
+
+        includesRequiredExtensions(&extensions);
+    }
+
+    bool includesRequiredExtensions(std::vector<VkExtensionProperties>* supportedExtensions) {
+        std::vector<VkExtensionProperties> extensions = *supportedExtensions;
+        std::vector<std::string> supportedExtensionNames;
+        supportedExtensionNames.reserve(extensions.size());
+        for (const auto& extension : extensions) {
+            std::string s(extension.extensionName);
+            supportedExtensionNames.push_back(s);
+        }
+
+        uint32_t extensionCount = 0;
+        const char** glfwExtensions = glfwGetRequiredInstanceExtensions(&extensionCount);
+
+        for (int i = 0; i < extensionCount; ++i) {
+            const std::string reqExtension(glfwExtensions[i]);
+
+            if(!containsString(&supportedExtensionNames, &reqExtension)) {
+                std::cerr << "Required extension '" << glfwExtensions[i] << "' is not supported!" << std::endl;
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool containsString(std::vector<std::string>* vec, const std::string* str) {
+        for (const auto& s : *vec) {
+            if(s.compare(*str)) return true;
+        }
+        return false;
     }
 };
 
